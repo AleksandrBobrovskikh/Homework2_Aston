c6c3596aa9ef2711d3b17525a690e645f1e3797c
AleksandrBobrovskikh <bobrosan@gmail.com>
2025-03-13 18:23:14 +0300

    Обработал замечания преподавателя

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,34 +2,10 @@
 public class Main {
     public static void main(String[] args) {
         try {
-            String result = Foots.getAnimal(-2);
-            System.out.println(result);
+            System.out.println(Foots.getAnimal(-2));
         } catch (FootException e) {
             System.out.println("Перехвачено исключение: " + e.getClass().getName() + " - " + e.getMessage());
             e.printStackTrace();
         }
     }
 }
-
-class FootException extends Exception {
-
-    public FootException(String message) {
-        super(message);
-    }
-}
-
-class Foots {
-
-    public static String getAnimal(int num) throws FootException {
-        if (num == 4) {
-            return "Это собака или кот";
-        } else if (num == 8) {
-            return "Это павук))";
-        } else if (num >= 0) {
-            return "Я не знаю что это!!!";
-        } else {
-            throw new FootException("Не может быть отрицательное количество ног");
-        }
-    }
-}
-
