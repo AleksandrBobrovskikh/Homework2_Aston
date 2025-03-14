8503b688f3b0ffd42408651753d2cc118842e72e
AleksandrBobrovskikh <bobrosan@gmail.com>
2025-03-14 20:53:47 +0300

    Создал свой арайлист

--- src/main/java/HW3/Main.java
+++ src/main/java/HW3/Main.java
@@ -1,4 +1,17 @@
 package HW3;
 
 public class Main {
+    public static void main(String[] args) {
+        MyArrayList<Integer> list = new MyArrayList<>();
+        list.add(5);
+        list.add(7);
+        list.add(9);
+        list.add(8);
+        list.add(6);
+        list.add(4);
+        list.print();
+        list.mergeSort();
+        list.print();
+
+    }
 }
